d7307590d1a9c3daff8222550816c275e5f6aae3
paulo <paulo_alexandre_slipknot@hotmail.com>
2023-03-23 13:51:11 -0400

    feat: implementa na função study_schedule() um meio para disponibilizar o melhor horário os conteúdos de estudo e a função se comportar como O(n)

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,15 @@
 def study_schedule(permanence_period, target_time):
+    """Faça o código aqui."""
+    if not target_time:
+        return None
+    online_users = 0
 
-    raise NotImplementedError
+    for start_time, output_time in permanence_period:
+
+        if not start_time or not output_time:
+            return None
+
+        if start_time <= target_time <= output_time:
+            online_users += 1
+
+    return online_users
