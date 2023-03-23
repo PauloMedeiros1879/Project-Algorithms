4d93bc7d0eae27745d6aa0b37e58f80c3cb92681
paulo <paulo_alexandre_slipknot@hotmail.com>
2023-03-23 14:26:38 -0400

    test(test_encrypt.py) implementa o teste respeitando a lógica informada

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,23 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    with pytest.raises(TypeError) as error:
+        encrypt_message("arroz doce", "é gostoso")
+
+    assert str(error.value) == "tipo inválido para key"
+
+    with pytest.raises(TypeError) as error:
+        encrypt_message(2, 1)
+
+    assert str(error.value) == "tipo inválido para message"
+
+    result_key = encrypt_message("arroz doce", 30)
+    assert result_key == "ecod zorra"
+
+    result_key = encrypt_message("arroz doce", 4)
+    assert result_key == "ecod z_orra"
+
+    result_key = encrypt_message("arroz doce", 4)
+    assert result_key != "rra_ecod zo"
