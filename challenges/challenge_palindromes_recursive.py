d3927fb3ead992b05835d37f8c39d5f30fe978a7
paulo <paulo_alexandre_slipknot@hotmail.com>
2023-03-24 08:15:40 -0400

    fix: modifica a ordem dos retornos

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,12 +1,13 @@
 def is_palindrome_recursive(word, low_index, high_index):
     """Faça o código aqui."""
+    # Retorne False se nenhuma palavra for passada por parâmetro
+    if word == "":
+        return False
     # Retorna True se a palavra passada por parâmetro for um palíndromo
     if low_index > high_index:
         return True
     # Retorne False se a palavra passada por parâmetro não for um palíndromo;
     if word[low_index] != word[high_index]:
         return False
-    # Retorne False se nenhuma palavra for passada por parâmetro
-    if word == "":
-        return False
+
     return is_palindrome_recursive(word, low_index + 1, high_index - 1)
