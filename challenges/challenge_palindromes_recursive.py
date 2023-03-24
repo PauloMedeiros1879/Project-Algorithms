55a9810d8e6d0a13e889d7e3a9a1572437a357c8
paulo <paulo_alexandre_slipknot@hotmail.com>
2023-03-24 07:51:42 -0400

    feat: adiciona os 3 retornos corretamente

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,7 +1,12 @@
 def is_palindrome_recursive(word, low_index, high_index):
     """Faça o código aqui."""
-    if low_index >= high_index:
+    # Retorna True se a palavra passada por parâmetro for um palíndromo
+    if low_index > high_index:
         return True
-    if word[low_index] != word[high_index] or word == "":
+    # Retorne False se a palavra passada por parâmetro não for um palíndromo;
+    if word[low_index] != word[high_index]:
+        return False
+    # Retorne False se nenhuma palavra for passada por parâmetro
+    if word == "":
         return False
     return is_palindrome_recursive(word, low_index + 1, high_index - 1)
