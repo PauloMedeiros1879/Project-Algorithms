317c4a32759645802e4e80e3dc90fee3039d133b
paulo <paulo_alexandre_slipknot@hotmail.com>
2023-03-24 10:55:51 -0400

    feat: Adicionar função is_anagram para verificar se duas strings são anagramas. Essa nova funcionalidade é implementada usando a função sort_string, que ordena as letras de cada string antes de compará-las. Isso permitirá que os usuários verifiquem se duas palavras são anagramas de maneira eficiente e fácil

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,3 +1,17 @@
+def sort_string(string):
+    string = list(string)
+    for i in range(len(string)):
+        for j in range(i, len(string)):
+            if string[j] < string[i]:
+                string[i], string[j] = string[j], string[i]
+    return "".join(string)
+
+
 def is_anagram(first_string, second_string):
     """Faça o código aqui."""
-    raise NotImplementedError
+    first_word = sort_string(list(first_string.casefold()))
+    second_word = sort_string(list(second_string.casefold()))
+    if first_word == second_word:
+        return (first_word, second_word, True)
+    else:
+        return (first_word, second_word, False)
